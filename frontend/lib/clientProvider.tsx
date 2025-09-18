2cb70c8832a87012515f35a86b520b220ebe7e6c
santosh kunwar <santoshkunwar@santoshs-MacBook-Air.local>
2025-09-18 14:54:02 +0545

    removed used use of toast provider

--- frontend/lib/clientProvider.tsx
+++ frontend/lib/clientProvider.tsx
@@ -1,11 +1,10 @@
 "use client";
 import { ReactNode } from "react";
-import { ToastProvider } from "./Toast"; // named import
 
 type ClientProvidersProps = {
   children: ReactNode;
 };
 
 export default function ClientProviders({ children }: ClientProvidersProps) {
-  return <ToastProvider>{children}</ToastProvider>;
+  return <>{children}</>;
 }
