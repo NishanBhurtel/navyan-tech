659748ead2046c8105043896e547c7ea87bc3ef7
santosh kunwar <santoshkunwar@santoshs-MacBook-Air.local>
2025-09-26 13:48:54 +0545

    added white listed origin

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -10,6 +10,7 @@ import { openApiDocument } from "./libs/swagger";
 import { createExpressEndpoints } from "@ts-rest/express";
 import morgan from "morgan";
 
+const allowedOrigins = process.env.WHITE_LISTED_ORIGINS?.split(",") || [];
 const app = express();
 
 app.use(express.json());
@@ -18,6 +19,14 @@ app.use(morgan("dev"));
 
 app.use(
   cors({
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        const msg = `The CORS policy for this site does not allow access from the specified Origin.`;
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
